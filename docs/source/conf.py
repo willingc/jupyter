767b7a6521ead64f13ff35c5772341b326ff248a
Carol Willing <carolcode@willingconsulting.com>
2016-03-26 14:49:12 -0700

    Add conf.py changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,8 @@
 import sys
 import os
 import shlex
-
+from jupyter_sphinx_theme import *
+init_theme()
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
